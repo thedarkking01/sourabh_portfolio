85357b9fa04dbb0e640b2e34f1aeb0c7b7446999
thedarkking01 <sourabhsinghbais52@gmail.com>
2024-06-30 23:39:31 +0530

    first commit

---
+++ data/index.ts
@@ -0,0 +1,312 @@
+export const navItems = [
+  { name: "About", link: "#about" },
+  { name: "Projects", link: "#projects" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: "I prioritize client collaboration, fostering open communication ",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/BB.jpg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I'm very flexible with time zone communications",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Tech enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building a JS Animation library",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "Gameonglow website",
+    des: "By this Gameonglow website user can buy skin care products for sports.",
+    img: "/gameon.png",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/wix.png", "/fm.svg"],
+    link: "https://www.gameonglow.com/",
+  },
+  {
+    id: 2,
+    title: "Sourabh Chat App",
+    des: "Simplify your Chat and file sharing experience with Sourabh Chat App. Seamlessly connect with colleagues and friends.",
+    img: "/chat.png",
+    iconLists: ["/re.svg", "/tail.svg", "/js.png", "/firebase.png"],
+    link: "https://sourabh-react-chat-app.netlify.app/",
+  },
+  {
+    id: 3,
+    title:
+      "FileStorage as SaaS platform ",
+    des: "where user can upload, delete,restore their files",
+    img: "/filedrive.png",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/clerk.png", "/convex.png"],
+    link: "https://ui.aiimg.com",
+  },
+  {
+    id: 4,
+    title: "Stack Tower Game",
+    des: "Ammazing .",
+    img: "/stack.png",
+    iconLists: [
+      "/next.svg",
+      "/tail.svg",
+      "/ts.svg",
+      "/clerk.png",
+      "/convex.png",
+    ],
+    link: "https://sourabh-stack-tower-game.netlify.app/",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Collaborating with Sourabh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sourabh enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sourabh is the ideal partner.",
+    name: "Kamal Sharma",
+    title: "Director of Gameonglow",
+  },
+  {
+    quote:
+      "Collaborating with Sourabh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sourabh enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sourabh is the ideal partner.",
+    name: "Kamal Sharma",
+    title: "Director of Gameonglow",
+  },
+  {
+    quote:
+      "Collaborating with Sourabh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sourabh enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sourabh is the ideal partner.",
+    name: "Kamal Sharma",
+    title: "Director of Gameonglow",
+  },
+  {
+    quote:
+      "Collaborating with Sourabh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sourabh enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sourabh is the ideal partner.",
+    name: "Kamal Sharma",
+    title: "Director of Gameonglow",
+  },
+  {
+    quote:
+      "Collaborating with Sourabh was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Sourabh enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Sourabh is the ideal partner.",
+    name: "Kamal Sharma",
+    title: "Director of Gameonglow",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    name: "Github",
+    img: "/git.svg",
+    nameImg: "/cloudName.svg",
+  },
+  {
+    id: 2,
+    name: "appwrite",
+    img: "/next.svg",
+    nameImg: "/appName.svg",
+  },
+  {
+    id: 3,
+    name: "HOSTINGER",
+    img: "/host.svg",
+    nameImg: "/hostName.svg",
+  },
+  {
+    id: 4,
+    name: "stream",
+    img: "/ts.svg",
+    nameImg: "/streamName.svg",
+  },
+  {
+    id: 5,
+    name: "docker.",
+    img: "/dock.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 6,
+    name: "docker.",
+    img: "/tail.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 7,
+    name: "docker.",
+    img: "/re.svg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 8,
+    name: "docker.",
+    img: "/firebase.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/node.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/ex.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/mongo.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/jest.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/mysql.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/py.jpg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/java.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/figma.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/wix.jpeg",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/convex.png",
+    nameImg: "/dockerName.svg",
+  },
+  {
+    id: 9,
+    name: "docker.",
+    img: "/clerk.png",
+    nameImg: "/dockerName.svg",
+  },
+];
+
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "MERN Stack Developer Intern",
+    desc: "Assisted in the development of website for GoodTechMind.",
+    className: "md:col-span-2",
+    thumbnail: "/exp1.svg",
+  },
+  {
+    id: 2,
+    title: "Web Developer Intern",
+    desc: "Designed and developed website for both Desktop & Mobile platforms for Goameonglow.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp2.svg",
+  },
+  {
+    id: 3,
+    title: "Freelance App Dev Project",
+    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/exp3.svg",
+  },
+  {
+    id: 4,
+    title: "Full Stack Developer",
+    desc: "Developed and maintained user-facing features using modern web technologies for Dotsquare company.",
+    className: "md:col-span-2",
+    thumbnail: "/exp4.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+    link: "https://github.com/thedarkking01",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+    link: "https://x.com/Sourabh05743314",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+    link: "https://www.linkedin.com/in/sourabh-singh-bais-50686823a/",
+  },
+];
