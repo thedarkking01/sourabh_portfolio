6483b241e0d9eb194a651d80758434e8ec9757f1
thedarkking01 <sourabhsinghbais52@gmail.com>
2024-07-04 18:44:47 +0530

    pic change

--- data/index.ts
+++ data/index.ts
@@ -13,7 +13,7 @@ export const gridItems = [
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
-    img: "/BB.jpg",
+    img: "/backg.jpg",
     spareImg: "",
   },
   {
