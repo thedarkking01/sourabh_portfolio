9fca5c4b5c91fd9adcaf43dca9ae7330386fc5b4
sourabh singh bais <sourabhsinghbais52@gmail.com>
2024-10-25 19:09:46 +0530

    favicon fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+
 import "./globals.css";
 import { ThemeProvider } from "./provider";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/s.jpg" sizes="any" />
+        <link rel="icon" href="/favicon.ico" sizes="any" />
       </head>
       <body className={inter.className}>
         <ThemeProvider
